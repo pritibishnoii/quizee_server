71e232043fad60937874e9e3f83ed4c3d94fba2a
priti bishnoi <108807403+pritibishnoii@users.noreply.github.com>
2024-08-26 19:16:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const Port = process.env.PORT || 8000;
 const userRoutes = require("./routes/user");
 const quizRoutes = require("./routes/quiz");
 
+const connectDb = require("./dbConnect");
+
 const app = express();
 
 // Enable Cors
@@ -21,22 +23,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Register Route
-app.use(
-	"/api/user",
-	async () => {
-		await connectDb();
-	},
-	userRoutes
-);
-app.use(
-	"/api/quiz",
-	async () => {
-		await connectDb();
-	},
-	quizRoutes
-);
+app.use("/api/user", userRoutes);
+app.use("/api/quiz", quizRoutes);
 app.get("*", async (req, res) => {
-	await connectDb();
 	res.status(200).json({
 		message: "Server is up and running",
 		createdAt: new Date().toISOString(),
@@ -48,20 +37,6 @@ app.use((err, req, res, next) => {
 	console.log(err.stack);
 	res.status(500).json({ error: "Internal server Error" });
 });
-
-let mongo_instance = null;
-
-const connectDb = async () => {
-	try {
-		if (mongo_instance) return mongo_instance;
-		mongo_instance = await mongoose.connect(process.env.MONGODB_URL);
-		console.log(`database connection successfully`);
-		return mongo_instance;
-	} catch (error) {
-		console.error(`Failed to connect to the database: ${error.message}`);
-		return null;
-	}
-};
 
 connectDb().then(() => {
 	app.listen(Port, () => {
